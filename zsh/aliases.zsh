c897ca3495b2c89a4479e38f2cf8ac5502b451cd
Lukas Pohlreich <pohlreichlukas@gmail.com>
2015-08-15 15:50:22 +0200

    [Zsh] Added some tmux aliases and options

---
+++ zsh/aliases.zsh
@@ -0,0 +1,16 @@
+# Generic aliases
+alias reload!='source ~/.zshrc'
+alias ls='ls --color=auto -l'
+alias df='df -h' # disk free
+alias du='du -h -c' # space usage for folder
+
+# Tmux aliases
+alias ta='tmux attach'
+alias tat='tmux attach -t'
+alias tls='tmux ls'
+alias tn='tmux new-session -s'
+
+# Livestreamer aliases
+alias sevadus='livestreamer twitch.tv/sevadus'
+alias dansgaming='livestreamer twitch.tv/dansgaming'
+alias towelliee='livestreamer twitch.tv/towelliee'
