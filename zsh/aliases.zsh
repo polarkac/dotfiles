95d8ea5cf16747f9b6ef9624349c9ee219f9c606
Lukas Pohlreich <pohlreichlukas@gmail.com>
2020-09-10 18:40:30 +0200

    [Zsh] Remove some aliases and Powerline

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,6 +1,6 @@
 # Generic aliases
 alias reload!='source ~/.zshrc'
-alias ls='ls --color=auto -l'
+alias ls='ls --color=auto -lh'
 alias df='df -h' # disk free
 alias du='du -h -c' # space usage for folder
 
@@ -9,8 +9,3 @@ alias ta='tmux attach'
 alias tat='tmux attach -t'
 alias tls='tmux ls'
 alias tn='tmux new-session -s'
-
-# Livestreamer aliases
-alias sevadus='livestreamer twitch.tv/sevadus'
-alias dansgaming='livestreamer twitch.tv/dansgaming'
-alias towelliee='livestreamer twitch.tv/towelliee'
